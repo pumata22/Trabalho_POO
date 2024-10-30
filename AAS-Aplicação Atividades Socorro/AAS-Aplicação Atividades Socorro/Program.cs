f9542e2896c2a9767cf2146c4943d426b31789f2
Mateus <pumata1928@gmail.com>
2024-10-30 16:38:01 +0000

    Classes

---
+++ AAS-Aplicação Atividades Socorro/AAS-Aplicação Atividades Socorro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAS
+{
+    internal class Program
+    {
+        static void Main()
+        {
+             void AddOcorrencias() 
+            {
+                //Tipo de ocorrencia
+                Console.Write("Qual e a ocorrencia?");
+                Console.Read();
+
+                if (Incendio) { }
+                else if (emergenciaMedica) { }
+                else if ()
+
+
+            }
+        }
+    }
+}
