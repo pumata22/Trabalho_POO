8de6b3f559ed0ef17d44885a7ac94a7b75ab8124
Julio <a29860@alunos.ipca.pt>
2025-01-05 21:08:49 +0000

    Update Program.cs

--- AAS-Aplicação Atividades Socorro/AAS-Aplicação Atividades Socorro/Program.cs
+++ AAS-Aplicação Atividades Socorro/AAS-Aplicação Atividades Socorro/Program.cs
@@ -1,5 +1,4 @@
 ﻿// Ignore Spelling: AAS
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,7 +27,7 @@ namespace AAS
         private int data;
         private int idade;
         private int nVitimas;
-        private int nCodu=0;
+        private int nCodu = 0;
         private int identificadorTipoEmergencia;
         private int nVeiculos;
         private int coordenadas1, coordenadas2;
@@ -42,13 +41,154 @@ namespace AAS
         private double areaArdida;
         #endregion
 
-            List<Ocorrencia> ocorrenciaList = new List<Ocorrencia>();
+        List<Ocorrencia> ocorrenciaList = new List<Ocorrencia>();
 
-        public List<Ocorrencia> AddOcorrencias()
+        public void EmergenciaMedica() {
+
+            char escolhaSexo;
+            Console.WriteLine("Emergência Médica");
+
+            Console.WriteLine("Qual o local?");
+            morada = Console.ReadLine();
+
+            Console.WriteLine("Quantas vitimas?");
+            nVitimas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Idade aproximada?");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sexo(M/F)");
+            escolhaSexo = char.Parse(Console.ReadLine());
+            if (escolhaSexo == 'M')
+            {
+                sexo = "Masculino";
+            }
+            else sexo = "Feminino";
+
+            Console.WriteLine("Observacoes");
+            observacoes = Console.ReadLine();
+
+            Pessoa pessoa = new Pessoa(nome, idade, dataNascimento, sexo, moradaPessoa);
+
+            Ocorrencia medica = new Ocorrencia(morada, veiculos, observacoes,
+                nVitimas, idade, nCodu, identificadorTipoEmergencia, nOperacionais, pessoa);
+            ocorrenciaList.Add(medica);
+            MENU();
+        }
+        public void Acidente()
+        {
+
+            Console.WriteLine("Acidente");
+
+            Console.WriteLine("Qual o local?");
+            morada = Console.ReadLine();
+
+            Console.WriteLine("Quantas vitimas?");
+            nVitimas = int.Parse(Console.ReadLine());
+
+            Pessoa pessoa1 = new Pessoa(nome, idade, dataNascimento, sexo, moradaPessoa);
+            Ocorrencia acidente = new Ocorrencia(morada, veiculos, observacoes,
+          nVitimas, nCodu, identificadorTipoEmergencia, coordenadas1, coordenadas2, nOperacionais,
+          pessoa1);
+            ocorrenciaList.Add(acidente);
+            MENU();
+        }
+        public void IncendioUrbano()
+        {
+            Console.WriteLine("Qual o local?");
+            morada = Console.ReadLine();
+
+            Console.WriteLine("Quantas vitimas?");
+            nVitimas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Observacoes");
+            observacoes = Console.ReadLine();
+
+            Console.WriteLine("Incêndio Urbano");
+            Ocorrencia urbano = new Ocorrencia(morada, veiculos, observacoes,
+                  nVitimas, identificadorTipoEmergencia, coordenadas1, coordenadas2, nOperacionais);
+            ocorrenciaList.Add(urbano);
+            MENU();
+        }
+        public void IncendioFlorestal()
+        {
+            Console.WriteLine("Incêndio Florestal");
+
+            Console.WriteLine("Qual o local?");
+            morada = Console.ReadLine();
+
+            Console.WriteLine("Observacoes");
+            observacoes = Console.ReadLine();
+
+            Ocorrencia florestal = new Ocorrencia(morada, veiculos, observacoes,
+                identificadorTipoEmergencia, coordenadas1, coordenadas2, nOperacionais, areaArdida);
+            ocorrenciaList.Add(florestal);
+            MENU();
+        }
+        public void IncendioIndustrial()
+        {
+            Console.WriteLine("Qual o local?");
+            morada = Console.ReadLine();
+
+            Console.WriteLine("Quantas vitimas?");
+            nVitimas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Observacoes");
+            observacoes = Console.ReadLine();
+
+            Console.WriteLine("Incêndio Industrial");
+            Ocorrencia industrial = new Ocorrencia(morada, veiculos, observacoes,
+                 nVitimas, identificadorTipoEmergencia, coordenadas1, coordenadas2, nOperacionais);
+            ocorrenciaList.Add(industrial);
+            MENU();
+        }
+        public void Assalto()
+        {
+            Console.WriteLine("Assalto");
+            Ocorrencia assalto = new Ocorrencia(morada, veiculos, observacoes, identificadorTipoEmergencia,
+                nOperacionais, coordenadas1, coordenadas2);
+            ocorrenciaList.Add(assalto);
+            MENU();
+        }
+        public void Agressao()
+        {
+
+            char escolhaSexoA;
+
+            Console.WriteLine("Agressão");
+            Console.WriteLine("Emergência Médica");
+
+            Console.WriteLine("Qual o local?");
+            morada = Console.ReadLine();
+
+            Console.WriteLine("Quantas vitimas?");
+            nVitimas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Idade aproximada?");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sexo(M/F)");
+            escolhaSexoA = char.Parse(Console.ReadLine());
+            if (escolhaSexoA == 'M')
+            {
+                sexo = "Masculino";
+            }
+            else sexo = "Feminino";
+
+            Console.WriteLine("Observacoes");
+            observacoes = Console.ReadLine();
+
+            Pessoa pessoa7 = new Pessoa(nome, idade, dataNascimento, sexo, moradaPessoa);
+            Ocorrencia agressao = new Ocorrencia(morada, veiculos, observacoes,
+               nVitimas, idade, nCodu, identificadorTipoEmergencia, nOperacionais, pessoa7);
+            ocorrenciaList.Add(agressao);
+            MENU();
+        }
+        public void MENU()
         {
             string escolha;
-            char escolhaSexo;
             Console.WriteLine("Qual o tipo da ocorrência?");
+            Console.WriteLine("Numero de ocorrencias:{0}",ocorrenciaList.Count );
             Console.WriteLine("1-Emergência Médica");
             Console.WriteLine("2-Acidente");
             Console.WriteLine("3-Incêndio Urbano");
@@ -57,147 +197,56 @@ namespace AAS
             Console.WriteLine("6-Assalto");
             Console.WriteLine("7-Agressão");
             Console.WriteLine();
-            escolha = Console.ReadLine(); // Lê como string
-         
+            escolha = Console.ReadLine();
+
             switch (escolha)
             {
+                
                 case "1":
-                    Console.WriteLine("Emergência Médica");
 
-                    Console.WriteLine("Qual o local?");
-                    morada = Console.ReadLine();
-
-                    Console.WriteLine("Quantas vitimas?");
-                    nVitimas = int.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Idade aproximada?");
-                    idade = int.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Sexo(M/F)");
-                    escolhaSexo = char.Parse(Console.ReadLine());
-                    if (escolhaSexo == 'M')
-                    {
-                     sexo = "Masculino";
-                    }
-                    else sexo = "Feminino";
-
-                    Console.WriteLine("Observacoes");
-                    observacoes = Console.ReadLine();
-
-                    Pessoa pessoa = new Pessoa( nome, idade, dataNascimento, sexo, moradaPessoa);
-
-                    Ocorrencia medica = new Ocorrencia(morada, veiculos, observacoes,
-                        nVitimas, idade, nCodu, identificadorTipoEmergencia, nOperacionais, pessoa);
-                    ocorrenciaList.Add(medica);
+                    EmergenciaMedica();
                     break;
+
                 case "2":
-                    Console.WriteLine("Acidente");
-
-                    Console.WriteLine("Qual o local?");
-                    morada = Console.ReadLine();
-
-                    Console.WriteLine("Quantas vitimas?");
-                    nVitimas = int.Parse(Console.ReadLine());
-
-                    Pessoa pessoa1 = new Pessoa(nome, idade, dataNascimento, sexo, moradaPessoa);
-                    Ocorrencia acidente = new Ocorrencia( morada,  veiculos,  observacoes,
-                  nVitimas, nCodu,  identificadorTipoEmergencia,  coordenadas1,  coordenadas2,  nOperacionais,
-                  pessoa1);
-                    ocorrenciaList.Add(acidente);
+                    Acidente();
                     break;
 
                 case "3":
-                    Console.WriteLine("Qual o local?");
-                    morada = Console.ReadLine();
-
-                    Console.WriteLine("Quantas vitimas?");
-                    nVitimas = int.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Observacoes");
-                    observacoes = Console.ReadLine();
-
-                    Console.WriteLine("Incêndio Urbano");
-                    Ocorrencia urbano = new Ocorrencia(morada, veiculos, observacoes,
-                          nVitimas, identificadorTipoEmergencia, coordenadas1, coordenadas2, nOperacionais);
-                    ocorrenciaList.Add(urbano);
+                   IncendioUrbano();
                     break;
 
                 case "4":
-                    Console.WriteLine("Incêndio Florestal");
-
-                    Console.WriteLine("Qual o local?");
-                    morada = Console.ReadLine();
-
-                    Console.WriteLine("Observacoes");
-                    observacoes = Console.ReadLine();
-
-                    Ocorrencia florestal = new Ocorrencia(morada, veiculos, observacoes,
-                        identificadorTipoEmergencia, coordenadas1, coordenadas2, nOperacionais, areaArdida);
-                    ocorrenciaList.Add(florestal);
-                        break;
+                    IncendioFlorestal();
+                    break;
 
                 case "5":
-                    Console.WriteLine("Qual o local?");
-                    morada = Console.ReadLine();
-
-                    Console.WriteLine("Quantas vitimas?");
-                    nVitimas = int.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Observacoes");
-                    observacoes = Console.ReadLine();
-
-                    Console.WriteLine("Incêndio Industrial");
-                    Ocorrencia industrial = new Ocorrencia(morada, veiculos, observacoes,
-                         nVitimas, identificadorTipoEmergencia, coordenadas1, coordenadas2, nOperacionais);
-                    ocorrenciaList.Add(industrial);
+                    IncendioIndustrial();
                     break;
 
                 case "6":
-                    Console.WriteLine("Assalto");
-                    Ocorrencia assalto = new Ocorrencia(morada, veiculos, observacoes, identificadorTipoEmergencia,
-                        nOperacionais, coordenadas1, coordenadas2);
-                    ocorrenciaList.Add(assalto);
+                    Assalto();
                     break;
+
                 case "7":
-                    Console.WriteLine("Agressão");
-                    Console.WriteLine("Emergência Médica");
-
-                    Console.WriteLine("Qual o local?");
-                    morada = Console.ReadLine();
-
-                    Console.WriteLine("Quantas vitimas?");
-                    nVitimas = int.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Idade aproximada?");
-                    idade = int.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Sexo(M/F)");
-                    escolhaSexo = char.Parse(Console.ReadLine());
-                    if (escolhaSexo == 'M')
-                    {
-                        sexo = "Masculino";
-                    }
-                    else sexo = "Feminino";
-
-                    Console.WriteLine("Observacoes");
-                    observacoes = Console.ReadLine();
-
-                    Pessoa pessoa7 = new Pessoa(nome, idade, dataNascimento, sexo, moradaPessoa);
-                    Ocorrencia agressao = new Ocorrencia(morada, veiculos, observacoes,
-                       nVitimas, idade, nCodu, identificadorTipoEmergencia, nOperacionais, pessoa7);
-                    ocorrenciaList.Add(agressao);
+                    Agressao();
                     break;
+
                 default:
                     Console.WriteLine("Opção inválida.");
                     break;
             }
 
+        }
+
+        public List<Ocorrencia> AddOcorrencias()
+        {
+            MENU();
             return ocorrenciaList;
         }
 
         void setOcorrencias()
-            {
-                List<string> tipoOcorrencia = new List<string>
+        {
+            List<string> tipoOcorrencia = new List<string>
             {
                 "Emergencia Medica",
                 "Incendio Urbano",
@@ -207,12 +256,12 @@ namespace AAS
                 "Assalto",
                 "Agressao"
             };
-            }
+        }
 
-            static void Main(string[] args)
-            {
-                Program p = new Program();
-                p.AddOcorrencias();
-            }
+        static void Main()
+        {
+            Program p = new Program();
+            p.AddOcorrencias();
+        }
     }
 }
